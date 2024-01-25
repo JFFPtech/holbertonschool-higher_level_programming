9131fb37736e94d41ea0c8c8e9780219853b8f74
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-24 23:31:06 -0400

    added format string

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(chr(i), end='')
+alpahbet = ""
+for letter in range(97, 123):
+    alpahbet += chr(letter)
+print("{}".format(alpahbet.lower()), end="")
