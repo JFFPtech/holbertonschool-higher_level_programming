f33c1658963ce8bc0448906c2b32983c61c425c8
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-25 22:58:27 -0400

    blocked execute

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-a = 10
-b = 5
+    res_add = add(a, b)
+    res_sub = sub(a, b)
+    res_mul = mul(a, b)
+    res_div = div(a, b)
 
-res_add = add(a, b)
-res_sub = sub(a, b)
-res_mul = mul(a, b)
-res_div = div(a, b)
-
-print("{} + {} = {}".format(a, b, res_add))
-print("{} - {} = {}".format(a, b, res_sub))
-print("{} * {} = {}".format(a, b, res_mul))
-print("{} / {} = {}".format(a, b, res_div))
+    print("{} + {} = {}".format(a, b, res_add))
+    print("{} - {} = {}".format(a, b, res_sub))
+    print("{} * {} = {}".format(a, b, res_mul))
+    print("{} / {} = {}".format(a, b, res_div))
