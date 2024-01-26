ddd64f31cf2c198e9a33435f700225c5f45cbb36
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-25 23:02:33 -0400

    improved readability, pycode, chmod

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     num_args = len(argv) - 1
-    print("{} {}{}".format(num_args, "argument" if num_args == 1 else "arguments", ":" if num_args > 0 else "."))
+    args_text = "argument" if num_args == 1 else "arguments"
+    punctuation = ":" if num_args > 0 else "."
+
+    print("{} {}{}".format(num_args, args_text, punctuation))
 
     for i in range(1, num_args + 1):
         print("{}: {}".format(i, argv[i]))
