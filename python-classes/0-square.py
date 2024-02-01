c312ab424cb5f24e66257ddee1f7569aa9cb4a50
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-01 19:25:03 -0400

    added documentation and pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Square class.
+
+This is a empty class that defines a square.
+
+"""
+
+
 class Square:
+    """Defines a square."""
+
     pass
+
 
 my_square = Square()
 print(type(my_square))
