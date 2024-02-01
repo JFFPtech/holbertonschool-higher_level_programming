acf011726372264ff250998d27310de7237f4ca2
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-01 19:57:59 -0400

    fixed traceback attribute error

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Square class.
+
+Contains class that defines a square, with initialization of size.
+Checks if the values are right, with setter and getter methods. Included area method to return the area of the square.
+
+"""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        """Initializes the size."""
+        self.size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+    @property
+    def size(self):
+        """Returns the current square size."""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Sets the square size."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
