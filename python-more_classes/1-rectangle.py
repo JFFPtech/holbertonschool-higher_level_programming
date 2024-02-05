f16dd28424962b0d01dee1db6bc08919056919f2
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-05 09:46:58 -0400

    eliminated redundant line, divided property/setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,28 +11,11 @@ class Rectangle:
         self.width = width
         self.height = height
     
-    def height(self):
-        """height: get height"""
-        return self.__height
-    @property
-    def height(self):
-        """height: set height"""
-        return self.__height
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def width(self):
-        """width: get width"""
-        return self.__width
     @property
     def width(self):
-        """width: set width"""
+        """Gets width of rectangle"""
         return self.__width
+    
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -40,3 +23,16 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """height: get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
