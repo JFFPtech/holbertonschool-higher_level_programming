588d18d05a6b5330eb61aa6d14b9e553230b95c3
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-07 14:41:36 -0400

    Update README.md and modify lookup.py and my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Module for MyList class."""
+
+
 class MyList(list):
-    """A class that inherits from list."""
+    """A class that inherits from a list""
     def print_sorted(self):
         """Prints the list, but sorted."""
-        print(sorted(self))
+        print_list = self[:]
+        print_list.sort()
+        print(print_list)
