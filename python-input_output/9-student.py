95350babd8fef97b742116e9a58b586c27330986
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-09 12:42:47 -0400

    created and initialized student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Student to JSON with filter"""
+
+
+class Student:
+    """Represents a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
