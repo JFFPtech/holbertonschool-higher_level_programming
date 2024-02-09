4c1374c1a7fb9bd3b4e0a51a075c53856d5def70
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-09 12:59:29 -0400

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a student"""
+
+
+class Student:
+    """Represents a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+            
+    def to_json(self, attrs=None):
+        """Gets the dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    new_dict[key] = getattr(self, key)
+            return new_dict
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key in json:
+            setattr(self, key, json[key])
