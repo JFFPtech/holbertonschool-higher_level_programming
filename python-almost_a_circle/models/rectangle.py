e3ddc60496435249ec735bd709451e6f7b06832a
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-12 14:31:36 -0400

    added area module

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,6 +70,10 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
 
 if __name__ == "__main__":
 
