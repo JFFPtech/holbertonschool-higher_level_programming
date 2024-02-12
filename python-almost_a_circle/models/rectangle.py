a7aa1bcf4cd61cce09f22cd25310540fd1591cb1
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-12 14:36:23 -0400

    added str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,13 +73,18 @@ class Rectangle(Base):
     def area(self):
         """Returns the area of the rectangle"""
         return self.__width * self.__height
-    
+
     def display(self):
         """Prints the rectangle with #"""
         for i in range(self.__y):
             print()
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """Returns the string representation of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
 
 
 if __name__ == "__main__":
