2336a307ad69a2001f4239868c81e9d8075d551e
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-12 15:09:25 -0400

    added square superclasss

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module with inherited Rectangle class"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the class"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns the string representation of the class"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
