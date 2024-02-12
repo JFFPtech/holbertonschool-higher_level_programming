30fed23008050fc52e0590c78431933a161a11c8
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-12 13:49:30 -0400

    indent fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,17 +7,17 @@ class Base:
     """Base class"""
 
 
-__nb_objects = 0
+    __nb_objects = 0
 
 
-def __init__(self, id=None):
-    """Initializes the class"""
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
-        
+    def __init__(self, id=None):
+        """Initializes the class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
 if __name__ == "__main__":
 
     b1 = Base()
