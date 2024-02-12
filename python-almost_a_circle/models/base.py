28faf6b7f7be2a6dd41268b22132e0c4d2eb6c41
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-12 18:52:53 -0400

    added classmethod to load from file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 
 
 import json
+
+
 class Base:
     """Base class"""
     __nb_objects = 0
@@ -15,14 +17,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
@@ -34,7 +36,7 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 jsonfile.write(cls.to_json_string(list_dicts))
-    
+
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""
         if json_string is None or json_string == "":
@@ -50,23 +52,17 @@ class Base:
             new = cls(1)
         new.update(**dictionary)
         return new
-    
+
+    @classmethod
     def load_from_file(cls):
         """Returns a list of instances"""
-        
-        import os.path
-
-        filename = cls.__name__ + ".json"
-        if os.path.exists(filename):
-            with open(filename, "r") as file:
-                json_string = file.read()
-        else:
+        file_name = "{}.json".format(cls.__name__)
+        try:
+            with open(file_name, "r") as jsonfile:
+                list_dicts = cls.from_json_string(jsonfile.read())
+            return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
             return []
-        obj_list = cls.from_json_string(json_string)
-        instance_list = []
-        for item in obj_list:
-            instance_list.append(cls.create(**item))
-        return instance_list
 
 
 if __name__ == "__main__":
