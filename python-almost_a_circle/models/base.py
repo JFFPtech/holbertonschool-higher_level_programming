61bb646089d7cfdf94432b44f40e90f0c131f5e6
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-12 18:21:57 -0400

    added ave to file def

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,21 +22,19 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        file_name = "{}.json".format(cls.__name__)
 
+        with open(file_name, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                jsonfile.write(cls.to_json_string(list_dicts))
 
 if __name__ == "__main__":
 
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
+    pass
