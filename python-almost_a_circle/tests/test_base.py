5dd355eba95ef08ad5eb0abfe13a56cfeebd0e06
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-16 13:36:19 -0400

    added header and fixed errors fromt test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,59 +1,74 @@
+#!/usr/bin/python3
+
+
+"""Defines unittests for base.py."""
+
+
+import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-class TestBase(unittest.TestCase):
-    def test_base__init__(self):
-        """Test Base initialization"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
 
-    def test_to_json_string(self):
-        """Test Base to_json_string method"""
-        b1 = Base()
-        self.assertEqual(Base.to_json_string(None), "[]")
-        self.assertEqual(Base.to_json_string([]), "[]")
-        self.assertEqual(Base.to_json_string([{"id": 1}]), '[{"id": 1}]')
+class TestBase_create(unittest.TestCase):
+    """Unittests for testing create method of Base class."""
 
-    def test_save_to_file(self):
-        """Test Base save_to_file method"""
-        b1 = Base()
-        b2 = Base()
-        Base.save_to_file([b1, b2])
-        with open("Base.json", "r") as file:
-            self.assertEqual(file.read(), '[{"id": 1}, {"id": 2}]')
 
-    def test_from_json_string(self):
-        """Test Base from_json_string method"""
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string(""), [])
-        self.assertEqual(Base.from_json_string('[{"id": 1}]'), [{"id": 1}])
+    def test_create_rectangle_original(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
 
-    def test_create(self):
-        """Test Base create method"""
-        r1 = Rectangle(3, 5, 1, 2, 10)
-        dictionary = r1.to_dictionary()
-        r2 = Rectangle.create(**dictionary)
-        self.assertEqual(str(r2), "[Rectangle] (10) 1/2 - 3/5")
 
-    def test_load_from_file(self):
-        """Test Base load_from_file method"""
-        b1 = Base()
-        b2 = Base()
-        Base.save_to_file([b1, b2])
-        instances = Base.load_from_file()
-        self.assertEqual(len(instances), 2)
-        self.assertEqual(instances[0].id, 1)
-        self.assertEqual(instances[1].id, 2)
+    def test_create_rectangle_new(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r2))
 
-if __name__ == '__main__':
+
+    def test_create_rectangle_is(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertIsNot(r1, r2)
+
+
+    def test_create_rectangle_equals(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertNotEqual(r1, r2)
+
+
+    def test_create_square_original(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertEqual("[Square] (7) 5/1 - 3", str(s1))
+
+
+    def test_create_square_new(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertEqual("[Square] (7) 5/1 - 3", str(s2))
+
+
+    def test_create_square_is(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertIsNot(s1, s2)
+
+
+    def test_create_square_equals(self):
+        s1 = Square(3, 5, 1, 7)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertNotEqual(s1, s2)
+
+if __name__ == "__main__":
     unittest.main()
