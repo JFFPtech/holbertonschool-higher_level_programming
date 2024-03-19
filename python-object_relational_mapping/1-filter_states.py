0a4e97d6e382c257d3cb874c9df4d00b99c786bf
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-03-19 08:17:47 -0700

    test for filter states, added if to print

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module to list all states from a database"""
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
