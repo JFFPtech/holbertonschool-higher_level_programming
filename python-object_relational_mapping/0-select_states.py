b06197bfa197a831c969f8bbcbae97247a10b598
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-03-19 08:56:18 -0700

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,10 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Establish a connection to the MySQL database
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve all rows from the `states` table
-    cursor.execute("SELECT * FROM `states`")
-
-    # Create a set to store printed states
-    printed_states = set()
-
-    # Fetch all rows and print each state
-    for state in cursor.fetchall():
-        # Check if the state has been printed before
-        if state[1] not in printed_states:
-            print(state)
-            # Add the state to the set of printed states
-            printed_states.add(state[1])
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
