257b9639537ee42ce7f739722aadbac2153eb648
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-05-25 19:12:22 -0700

    added promises for json data

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const listMovies = document.getElementById('list_movies');
     fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(movie => {
+                const listItem = document.createElement('li');
+                listItem.textContent = movie.title;
+                listMovies.appendChild(listItem);
+            });
